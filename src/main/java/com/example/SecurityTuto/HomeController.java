62901fe86a200b2564a960689f17bd63d28d7fb2
Anuran Pradhan <anuranpradhan002@gmail.com>
2025-07-19 11:55:29 +0530

    Initial commit

---
+++ src/main/java/com/example/SecurityTuto/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.SecurityTuto;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/index")
+    public String home(){
+        return "Home";
+    }
+}
---
+++ src/main/java/com/example/SecurityTuto/SecurityTutoApplication.java
@@ -0,0 +1,13 @@
+package com.example.SecurityTuto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityTutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityTutoApplication.class, args);
+	}
+
+}
